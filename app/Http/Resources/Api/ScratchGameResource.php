f1fcbc2626ccb821e6f45734d3c4ed1578d13a02
saadnabil <saadnabil00@gmail.com>
2024-02-18 12:26:54 +0200

    fdf

--- app/Http/Resources/Api/ScratchGameResource.php
+++ app/Http/Resources/Api/ScratchGameResource.php
@@ -23,6 +23,13 @@ class ScratchGameResource extends JsonResource
             'open_cell_index' => $this->open_cell_index,
             'time_open_cell_index' => $this->time_open_cell_index,
             'allow_to_scratch' => $this->open_cell_index == 0 ? 1 : (Carbon::now(auth()->user()->country->timezone)->diffInMinutes(Carbon::createFromFormat('h:i a',$this->time_open_cell_index)) > 60 ? 1 : 0) ,
+            'cell_one_status' =>  $this->open_cell_index >= 1 ? 1 : 0,
+            'cell_two_status' => $this->open_cell_index >= 2 ? 1 : 0,
+            'cell_three_status' => $this->open_cell_index >= 3 ? 1 : 0,
+            'cell_four_status' => $this->open_cell_index >= 4 ? 1 : 0,
+            'cell_five_status' => $this->open_cell_index >=  5? 1 : 0,
+
+
         ];
     }
 }
