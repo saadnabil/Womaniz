c928589cc7fe980841ca1846cbe52a8701fef94a
saadnabil <saadnabil00@gmail.com>
2024-03-27 12:01:52 +0200

    fddfdf

--- app/Http/Resources/Api/ImageResource.php
+++ app/Http/Resources/Api/ImageResource.php
@@ -16,7 +16,7 @@ class ImageResource extends JsonResource
     {
         $data = [
             'id' => $this->id,
-            'image' => url('storage/ '.$this->image),
+            'image' => url('storage/'.$this->image),
         ];
         return $data;
     }
