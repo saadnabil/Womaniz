6821aeeb80683ef1b4dd82b5c0d03fb5eb5e1138
saadnabil <saadnabil00@gmail.com>
2024-04-18 17:27:42 +0200

    fdf

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,9 @@
 
 use App\Models\Country;
 use App\Models\Coupon;
-use App\Models\ScratchGame;
-use App\Models\ScratchGameUser;
-use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
