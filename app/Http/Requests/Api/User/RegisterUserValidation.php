7ae8aacec4e6a7eeb90834267223eb511dafaf7f
saadnabil <saadnabil00@gmail.com>
2024-04-23 13:36:40 +0200

    fdf

--- app/Http/Requests/Api/User/RegisterUserValidation.php
+++ app/Http/Requests/Api/User/RegisterUserValidation.php
@@ -34,7 +34,7 @@ class RegisterUserValidation extends AbstractFormRequest
             'confirmpassword' => ['required', 'string' ,'min:8', 'same:password'],
             'country_id' => ['required', 'numeric','in:'.$countryIds],
             'otp' => ['nullable', 'digits:4'],
-            'gender' => ['required', 'string', 'in:Femail,Male,Other'],
+            'gender' => ['required', 'string', 'in:Female,Male,Other'],
         ];
     }
 }
