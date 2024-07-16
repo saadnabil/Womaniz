99d8986c9ad3b5eb46d49c1fed88eacc0385639b
saadnabil <saadnabil00@gmail.com>
2024-07-16 11:33:33 +0300

    fdf

--- app/Http/Controllers/Api/Dashboard/SizesController.php
+++ app/Http/Controllers/Api/Dashboard/SizesController.php
@@ -2,20 +2,20 @@
 namespace App\Http\Controllers\Api\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\CategoryResource;
-use App\Http\Resources\Api\SizeResource;
+use App\Http\Resources\SizeResource;
 use App\Http\Traits\ApiResponseTrait;
 use App\Models\Category;
 use App\Models\Size;
+use Illuminate\Http\Request;
 
 class SizesController extends Controller
 {
     use ApiResponseTrait;
     public function index(){
         $sizes = Size::get();
+        if(request()->has('product_sub_type')){
+            $sizes = $sizes->whereIn('product_sub_type',['all',request('product_sub_type')]);
+        }
         return $this->sendResponse(SizeResource::collection($sizes));
-    }
-
-    public function getSizesCategory(){
-        $sizes = Size::get();
     }
 }
