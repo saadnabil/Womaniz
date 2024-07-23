28306953037ccfc375e8fba04afb8420a8582f9b
aymanmnader <109459551+aymanmnader@users.noreply.github.com>
2024-07-23 19:06:58 +0300

    bulk upload update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -97,49 +97,49 @@ class Product extends Model
     }
 
 
-    // protected static function boot()
-    // {
-    //     parent::boot();
-    //     static::created(function ($model) {
-    //         activity()
-    //             ->performedOn($model)
-    //             ->causedBy(auth()->user())
-    //             ->withProperties($model->getAttributes())
-    //             ->log('Create');
-    //     });
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($model) {
+            activity()
+                ->performedOn($model)
+                ->causedBy(auth()->user())
+                ->withProperties($model->getAttributes())
+                ->log('Create');
+        });
 
-    //     static::updated(function ($model) {
-    //         $originalAttributes = $model->getOriginal();
+        static::updated(function ($model) {
+            $originalAttributes = $model->getOriginal();
 
-    //         $attributes=[];
-    //         foreach ($model->getDirty() as $attribute => $newValue) {
-    //             $oldValue = $originalAttributes[$attribute] ?? null;
+            $attributes=[];
+            foreach ($model->getDirty() as $attribute => $newValue) {
+                $oldValue = $originalAttributes[$attribute] ?? null;
 
-    //             if ($oldValue !== $newValue) {
-    //                 $attributes[$attribute]['old']=$oldValue;
-    //                 $attributes[$attribute]['new']=$newValue;
-    //             }
-    //         }
+                if ($oldValue !== $newValue) {
+                    $attributes[$attribute]['old']=$oldValue;
+                    $attributes[$attribute]['new']=$newValue;
+                }
+            }
 
-    //         activity()
-    //             ->performedOn($model)
-    //             ->causedBy(auth()->user())
-    //             ->withProperties($attributes)
-    //             ->log('Updated');
+            activity()
+                ->performedOn($model)
+                ->causedBy(auth()->user())
+                ->withProperties($attributes)
+                ->log('Updated');
 
 
-    //     });
+        });
 
-    //     static::deleting(function ($model) {
-    //         $attributes = $model->getAttributes();
+        static::deleting(function ($model) {
+            $attributes = $model->getAttributes();
 
-    //         activity()
-    //             ->performedOn($model)
-    //             ->causedBy(auth()->user())
-    //             ->withProperties($attributes)
-    //             ->log('Delete');
-    //     });
-    // }
+            activity()
+                ->performedOn($model)
+                ->causedBy(auth()->user())
+                ->withProperties($attributes)
+                ->log('Delete');
+        });
+    }
 
 
 
