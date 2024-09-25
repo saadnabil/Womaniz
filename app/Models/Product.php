370c2f5fa07d00cd54302dfcbf27a5ba4b3ffe8d
saadnabil <saadnabil00@gmail.com>
2024-09-25 16:33:43 +0300

    fgf

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -64,15 +64,16 @@ class Product extends Model
         return $this->return_order_desc_ar;
     }
 
-    // public function getThumbnailAttribute(){
-    //     if (filter_var($this->thumbnail, FILTER_VALIDATE_URL)) {
-    //         // If the image is a valid URL, return it directly
-    //         return $this->thumbnail;
-    //     } else {
-    //         // If the image is not a URL, assume it's a file path and return it with the asset helper
-    //         return url('storage/' . $this->thumbnail);
-    //     }
-    // }
+    public function getThumbnailAttribute(){
+        return $this->thumbnail;
+        // if (filter_var($this->thumbnail, FILTER_VALIDATE_URL)) {
+        //     // If the image is a valid URL, return it directly
+        //     return $this->thumbnail;
+        // } else {
+        //     // If the image is not a URL, assume it's a file path and return it with the asset helper
+        //     return url('storage/' . $this->thumbnail);
+        // }
+    }
 
     public function getMaterialAttribute(){
         $lang = app()->getLocale();
