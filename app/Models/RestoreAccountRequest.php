fd0951173b1f946a5f60305dee376c3d5c91b262
saadnabil <saadnabil00@gmail.com>
2024-10-17 11:41:31 +0300

    dff

--- app/Models/RestoreAccountRequest.php
+++ app/Models/RestoreAccountRequest.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RestoreAccountRequest extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $guarded = [];
+
+    protected $date = ['deleted_at'];
+
 
     public function user(){
         return $this->belongsTo(User::class)->withTrashed();
