74bf0295635e150d64864a021a7f91dcdbc6e7cc
saadnabil <saadnabil00@gmail.com>
2024-10-31 12:45:23 +0300

    fg

--- app/Http/Controllers/Api/User/SalonController.php
+++ app/Http/Controllers/Api/User/SalonController.php
@@ -57,7 +57,6 @@ class SalonController extends Controller
                 return $service;
             });
         }
-        dd('fdf');
         return $this->sendResponse(ServicesExpertsResource::collection($services));
     }
 
