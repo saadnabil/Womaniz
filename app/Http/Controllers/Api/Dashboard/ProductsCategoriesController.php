6dc2ecb669f4810f570a3ae072cf676ab88782ee
saadnabil <saadnabil00@gmail.com>
2024-11-14 13:25:43 +0200

    gfg

--- app/Http/Controllers/Api/Dashboard/ProductsCategoriesController.php
+++ app/Http/Controllers/Api/Dashboard/ProductsCategoriesController.php
@@ -6,20 +6,33 @@ use App\Http\Requests\Dashboard\UpdateProductCategoriesValidation;
 use App\Http\Traits\ApiResponseTrait;
 use App\Models\CategoryProduct;
 use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductsCategoriesController extends Controller
 {
     use ApiResponseTrait;
     public function updateCategories(UpdateProductCategoriesValidation $request, Product $product){
         $data = $request->validated();
-        foreach($data['categories'] as $category){
-            
-            CategoryProduct::firstorcreate([
-                'product_id' => $product->id,
-                'category_id' => $category,
+        try{
+            DB::beginTransaction();
+            $data['status'] ? $data['status'] : 'live';
+            $product->update([
+                'status' => 'live',
             ]);
+            foreach($data['categories'] as $category){
+                CategoryProduct::firstorcreate([
+                    'product_id' => $product->id,
+                    'category_id' => $category,
+                ]);
+            }
+            DB::commit();
+            return $this->sendResponse([]);
+        }catch(Exception $ex){
+            DB::rollBack();
+            return $this->sendResponse(['error' => 'An error occured !'], 'fail' , 400);
         }
-        return $this->sendResponse([]);
+       
     }
 }
